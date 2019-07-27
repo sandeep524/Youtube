2096bf1b6e41b1e27ae8dbb59641d5dde2026f28
sandeep524 <36640058+sandeep524@users.noreply.github.com>
2019-07-27 13:03:37 +0530

    Update updated.py
    changed some classes which got updated!

--- Whatsapp Automation/updated.py
+++ Whatsapp Automation/updated.py
@@ -3,18 +3,18 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get('https://web.whatsapp.com/')
 
-name = input('Enter the name of user or group : ')
-msg = input('Enter your message : ')
-count = int(input('Enter the count : '))
+name = raw_input('Enter the name of user or group : ')
+msg = raw_input('Enter your message : ')
+count = int(raw_input('Enter the count : '))
 
-input('Enter anything after scanning QR code')
+raw_input('Enter anything after scanning QR code')
 
 user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
 user.click()
 
-msg_box = driver.find_element_by_class_name('_2S1VP')
+msg_box = driver.find_element_by_class_name('_13mgZ')
 
 for i in range(count):
     msg_box.send_keys(msg)
-    button = driver.find_element_by_class_name('_35EW6')
+    button = driver.find_element_by_class_name('_3M-N-')
     button.click()
